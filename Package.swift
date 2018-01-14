a8191d3b9481b343b65bb160d60c1f48a1c1cb6e
Ed Moss <edmoss@Eds-Mac.local>
2018-01-14 05:41:32 +0100

    commit

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Cpcl",
-    pkgConfig: "pcl_common-1.7",
+    pkgConfig: "pcl_common-1.8",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
